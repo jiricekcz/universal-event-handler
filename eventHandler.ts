4a8c1b39d37762f31000465e60274c0b5d0fda84
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-02-11 12:05:59 +0100

    namespace conversion

--- eventHandler.ts
+++ eventHandler.ts
@@ -1,21 +1,23 @@
 const handlers: any = {};
- 
-export function on(event: eventID, handler: (...args: any[]) => void) {
-    if (!(handlers[event] instanceof Array)) handlers[event] = [];
-    handlers[event].push(handler);
-}
-export function emit(event: eventID, ...args: any[]): void {
-    if (!(handlers[event] instanceof Array)) return void (handlers[event] = []);
-    for (const func of handlers[event]) {
-        func(...args);
+namespace EventHandler {
+    function on(event: eventID, handler: (...args: any[]) => void) {
+        if (!(handlers[event] instanceof Array)) handlers[event] = [];
+        handlers[event].push(handler);
     }
-}
-export function clear(event: eventID): void {
-    handlers[event] = [];
-}
-export function getHandlers(event: eventID): Array<(...args: any[]) => void> {
-    return handlers[event];
+    function emit(event: eventID, ...args: any[]): void {
+        if (!(handlers[event] instanceof Array)) return void (handlers[event] = []);
+        for (const func of handlers[event]) {
+            func(...args);
+        }
+    }
+    function clear(event: eventID): void {
+        handlers[event] = [];
+    }
+    function getHandlers(event: eventID): Array<(...args: any[]) => void> {
+        return handlers[event];
+    }
+    type eventID = string;
 }
 
-export type eventID = string;
+export = EventHandler;
 
